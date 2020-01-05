93d711c7f4a7c747fc12ae5449680115188302b3
Benjamin Huang <bhuang@rms.com>
2020-01-05 12:23:38 -0800

    add read sql

--- pandas/load-data.py
+++ pandas/load-data.py
@@ -5,6 +5,14 @@ import pandas as pd
 filename = 'data/car_financing.csv'
 df = pd.read_csv(filename)
 
+filename = 'data/state-abbrevs.csv'
+df = pd.read_csv(filename, sep=',', header=None, names=['column1','column2'])
+
 # Load excel
 filename = 'data/car_financing.xlsx'
 df = pd.read_excel(filename)
+
+# Load mysql
+import pymysql
+conn = pymysql.connect(host='127.0.0.1', user='root', password='root', database='test')
+data = pd.read_sql('select * from mytable', con=conn)
